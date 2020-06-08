f177810c187e994d388eabd6139402d88afaa41d
francisco tena <ftenaf@yahoo.es>
2020-06-08 18:19:16 +0100

    fixed api service now the repository is public

--- lib/services/api/api_service_impl.dart
+++ lib/services/api/api_service_impl.dart
@@ -1,17 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 
 import 'package:covid_buster_lite/logic/models/topic.dart';
 import 'package:covid_buster_lite/services/api/api_service.dart';
 import 'package:covid_buster_lite/services/common/utils.dart';
+import 'package:http/http.dart' as http;
 
 class ApiServiceImpl implements ApiService {
   @override
   Future<List<Topic>> fetchTopics(String locale) async {
-    //TODO get locale file
-    String url =
-        'https://raw.githubusercontent.com/ftenaf/covid_buster_lite/master/assets/quizzes.json?token=AADNMWSAPN24PSPZS3NDYIS6XVXXS';
+    String url = 'https://raw.githubusercontent.com/ftenaf/covid_buster_lite/master/assets/data/quizzes_${locale}.json';
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body);

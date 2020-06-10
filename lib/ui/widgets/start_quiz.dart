1949dbe20af6930d325c545e4a5e6da2aab48d43
francisco tena <ftenaf@yahoo.es>
2020-06-10 21:24:27 +0100

    rounded buttons

--- lib/ui/widgets/start_quiz.dart
+++ lib/ui/widgets/start_quiz.dart
@@ -29,23 +29,30 @@ class StartPage extends StatelessWidget {
             quiz.description,
             style: TextStyle(fontSize: 25),
           )),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton.icon(
-                onPressed: state.nextPage,
-                label: Text(
-                  'quiz.start'.tr(),
-                  style: TextStyle(fontSize: 45),
-                ),
-                icon: Icon(
-                  Icons.poll,
-                  size: 45,
-                ),
-                color: Colors.green,
-              )
-            ],
-          )
+          RaisedButton(
+              elevation: 4,
+              padding: EdgeInsets.all(25.0),
+              onPressed: state.nextPage,
+              color: Colors.green[500],
+              hoverColor: Colors.orange,
+              highlightColor: Colors.pink,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  side: BorderSide(color: Colors.grey[800], width: 1, style: BorderStyle.solid)),
+              child: Row(
+                children: [
+                  Icon(Icons.poll),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 16),
+                      child: Text(
+                        'quiz.start'.tr(),
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );

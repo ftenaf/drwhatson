ee0fbc0f477ba36ef084ed3748e4484fe5491e3f
ftenaf <ftenaf@yahoo.es>
2020-06-12 09:06:12 +0000

    Deploying to gh-pages from  @ 91d5422e052aad02ea1422abb369dd0b479bde05 🚀

---
+++ assets/assets/data/about_en_US.md
@@ -0,0 +1,13 @@
+# Warning
+
+This approach is only a framework from reﬂexión to analyze how carefully or late we can adapt the general measures to our individual risk. 
+
+The risk factors are drawn from the studies to date with COVID-19 and the known physiopathology risk factors for severe viral infections, and in particular for SARS-CoV-2. 
+
+These thoughts do not replace medical advice, if you have a pathology of special risk, sure that your doctor can clarify the doubts to you better. 
+
+You may think that these are excessively prudent recommendations, but in my experience they are not: the recommendation to go 1-2 weeks behind is not so aggressive and with all that remains to be known, if I am to sin, it will be prudent. 
+
+And it will be for your own good.
+
+[Summary of the situation](https://www.notion.so/Semana-4-10-Mayo-fccb02e9641b496a81e59350fa9c15f7)
---
+++ flutter_service_worker.js
@@ -0,0 +1,175 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/data/about_en_US.md": "dc9975a827016b1b6634ddad287632e0",
+"assets/assets/data/quizzes_es_ES.json": "5535b10018c57d8d9d8fa83b4aeb2667",
+"assets/assets/data/quizzes_en_US.json": "e857b2eaa5da38bf02937ca7e5dba88c",
+"assets/assets/data/about_es_ES.md": "b42ce0897181dab21c44857063ce2969",
+"assets/assets/right.flr": "88397cfc608b56ce81cd278fef966b85",
+"assets/assets/img/covid.png": "e24be87c6de3021f22d2cb88f6e75b79",
+"assets/assets/wrong.flr": "46f19954b51f4a0bac19209a4b85c6d4",
+"assets/assets/languages/en-US.json": "689673de0b8e5cb13f4c103ea5709b5b",
+"assets/assets/languages/es-ES.json": "2587403eb678918339a5867548792b1c",
+"assets/assets/languages/de-DE.json": "774a74ee61336bd269c73f437957ec4b",
+"assets/assets/success.flr": "584a266bbf6c2020d5f0fa33a753bcb8",
+"assets/NOTICES": "6aae565999f162877c231ebf1461db09",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
+"assets/packages/easy_localization/i18n/en-US.json": "5bd908341879a431441c8208ae30e4fd",
+"assets/packages/easy_localization/i18n/en.json": "5bd908341879a431441c8208ae30e4fd",
+"assets/packages/easy_localization/i18n/ar.json": "acc0a8eebb2fcee312764600f7cc41ec",
+"assets/packages/easy_localization/i18n/ar-DZ.json": "acc0a8eebb2fcee312764600f7cc41ec",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/AssetManifest.json": "a624c020ef3dd2e1506fdf3f17ffca9f",
+"main.dart.js": "0a5905d23538206c1fd17c05553a33a8",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "337398b02e9bc0a492ac1aa3ce85f5b0",
+"index.html": "258c292340109b28ac7188ff04ecdaa6",
+"/": "258c292340109b28ac7188ff04ecdaa6",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "/",
+"main.dart.js",
+"index.html",
+"assets/LICENSE",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      // Provide a no-cache param to ensure the latest version is downloaded.
+      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'no-cache'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#')) {
+    key = '/';
+  }
+  // If the URL is not the the RESOURCE list, skip the cache.
+  if (!RESOURCES[key]) {
+    return event.respondWith(fetch(event.request));
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache. Ensure the resources are not cached
+        // by the browser for longer than the service worker expects.
+        var modifiedRequest = new Request(event.request, {'cache': 'no-cache'});
+        return response || fetch(modifiedRequest).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.message == 'skipWaiting') {
+    return self.skipWaiting();
+  }
+
+  if (event.message = 'downloadOffline') {
+    downloadOffline();
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey in Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.add(resourceKey);
+    }
+  }
+  return Cache.addAll(resources);
+}
